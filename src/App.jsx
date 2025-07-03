8428b348976dd62bb81adc4e9c132fad042ba7ce
Gabriel Canto <161658338+Bielartur@users.noreply.github.com>
2025-07-03 16:53:30 -0300

    feat: update main container on upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import MainContainer from "./components/MainContainer";
-import BasicContainer from "./components/BasicContainer"
 import InfoContainer from "./components/InfoContainer";
 
 const App = () => {
+  const [data, setData] = useState(null);
+
   return (
     <div className="w-2xl">
       <h1 className="text-3xl text-[var(--primary)] mb-6">
         Bem vindo ao site para automatizar suas finanças
       </h1>
       <MainContainer>
-        <InfoContainer
-          info="Selecione ou arraste o arquivo para aqui"
-        />
+        {data ? (
+          <pre className="whitespace-pre-wrap text-left">
+            {JSON.stringify(data, null, 2)}
+          </pre>
+        ) : (
+          <InfoContainer
+            info="Selecione ou arraste o arquivo para aqui"
+            onFileUploaded={setData}
+          />
+        )}
       </MainContainer>
     </div>
   );
